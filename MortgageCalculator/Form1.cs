350b1a3ca70eb1c0bc6376f2239f9749ab2cf637
Sean Kelley <seannkelleyy1@gmail.com>
2022-11-21 21:41:38 -0500

    Work from day 2

--- MortgageCalculator/Form1.cs
+++ MortgageCalculator/Form1.cs
@@ -13,7 +13,7 @@ namespace MortgageCalculator
 {
     public partial class Form1 : Form
     {
-        List<PaymentModel> payments= new List<PaymentModel>();
+        List<PaymentModel> payments = new List<PaymentModel>();
         MortgageModel mortgage = new MortgageModel();
         public Form1()
         {
@@ -22,8 +22,13 @@ namespace MortgageCalculator
 
         private void submitButton_Click(object sender, EventArgs e)
         {
+
+            StringBuilder query = new StringBuilder("insert into Mortgage (PaymentNumber, BeginningBalance, PaymentAmount, Principal, Interest, InterestPaid, EndingBalance) values ");
+            payments.Clear();
             SQLAccess.ClearTable();
             mortgageAmortization.Rows.Clear();
+
+
             if (amountTextBox.Text.Length > 0 && lengthTextBox.Text.Length > 0 && rateTextBox.Text.Length > 0)
             {
                 Boolean isdouble = double.TryParse(amountTextBox.Text, out double amount);
@@ -37,6 +42,7 @@ namespace MortgageCalculator
                 }
                 mortgage.InterestRate = Convert.ToDouble(rateTextBox.Text) / 1200;
                 mortgage.LoanLength = Convert.ToInt32(lengthTextBox.Text) * 12;
+
                 decimal monthlyPayment = mortgage.CalculatePaymentAmount(mortgage.LoanAmount, mortgage.InterestRate, mortgage.LoanLength);
                 monthlyPaymentAmountLabel.Text = monthlyPayment.ToString("C2");
 
@@ -44,20 +50,27 @@ namespace MortgageCalculator
                 for (int i = 0; i < length; i++)
                 {
                     payments.Add(PaymentModel.CalculatePayment(mortgage.LoanAmount, mortgage.LoanLength, mortgage.InterestRate, i + 1, mortgage.PaidInterest, monthlyPayment));
-                    SQLAccess.SavePayment(payments[i]);
+                    query.Append($"({payments[i].PaymentNumber}, {payments[i].BeginningBalance}, {payments[i].PaymentAmount}, {payments[i].Principal}, {payments[i].Interest}, {payments[i].InterestPaid}, {payments[i].EndingBalance}), ");
+
                     mortgage.LoanAmount -= payments[i].Principal;
                     mortgage.LoanLength--;
                     mortgage.PaidInterest += payments[i].Interest;
-                    List<PaymentModel> payment = SQLAccess.LoadPayment();
-                    mortgageAmortization.Rows.Add(payment[i].PaymentNumber, payment[i].BeginningBalance.ToString("C2"), payment[i].PaymentAmount.ToString("C2"), payment[i].Principal.ToString("C2"), payment[i].Interest.ToString("C2"), payment[i].InterestPaid.ToString("C2"), payment[i].EndingBalance.ToString("C2"));
+                    
                 }
 
+                query.Remove(query.Length - 2, 2);
+                SQLAccess.SavePayment(query.ToString());
+
+                for (int i = 0; i < length; i++)
+                {
+                    List<PaymentModel> payment = SQLAccess.LoadPayment();
+                    mortgageAmortization.Rows.Add(payments[i].PaymentNumber, payment[i].BeginningBalance.ToString("C2"), payment[i].PaymentAmount.ToString("C2"), payment[i].Principal.ToString("C2"), payment[i].Interest.ToString("C2"), payment[i].InterestPaid.ToString("C2"), payment[i].EndingBalance.ToString("C2"));
+                }
             }
             else
             {
                 monthlyPaymentAmountLabel.Text = "Please Enter Your Loan Information Above";
             }
-            SQLAccess.ClearTable();
         }
 
         private void resetButton_Click(object sender, EventArgs e)
